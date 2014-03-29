de4639e18e5b40216eedf0dbbe09d1342650e053
Jason Fehr <jasonmfehr@server.fake>
2014-03-29 10:47:25 -0500

    finished FileInputSourceReader and unit test

---
+++ src/test/java/com/jfehr/combiner/input/FileInputSourceReaderTest.java
@@ -0,0 +1,14 @@
+package com.jfehr.combiner.input;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileInputSourceReaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
