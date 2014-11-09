d0424dfdb06d6bbf74964eb5b894227978026e44
Jason Fehr <jasonmfehr@server.fake>
2014-11-09 14:28:37 -0600

    added additional asserts to ensure the integration tests that are expected to fail actually failed for the correct reason for #8

---
+++ src/it/it-fail-invalid-inputsourcereader/verify.groovy
@@ -0,0 +1,5 @@
+//Could not instantiate object with class com.jfehr.combiner.input.ThisReaderDoesNotExist
+
+String buildLog = new File(basedir.absolutePath + "/build.log").text
+
+if(!buildLog.contains("combine failed: Could not instantiate object with class com.jfehr.combiner.input.ThisReaderDoesNotExist")) throw new RuntimeException("integration test build failed for an error that was different than expected")
