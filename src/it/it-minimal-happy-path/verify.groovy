a6c5fe3ef44788c52421c005d045a290c615e8ec
Jason Fehr <jasonmfehr@server.fake>
2014-11-09 14:42:55 -0600

    added transformer to minimal happy path test to ensure testing of all stages of the pipeline for #8

--- src/it/it-minimal-happy-path/verify.groovy
+++ src/it/it-minimal-happy-path/verify.groovy
@@ -1,7 +1,9 @@
 String actual = new File(basedir.absolutePath + "/target/actual-result.txt").text
 
+if(actual.contains("\r|\n")) throw new RuntimeException("expected zero newline characters but found at least one")
+
 for(int i=1; i<=3; i++){
-    String fileText = new File(basedir.absolutePath + "/src/main/resources/file" + i + ".txt").text
+    String fileText = new File(basedir.absolutePath + "/src/main/resources/file" + i + ".txt").text.replaceAll("\r|\n", "")
     
     if(!actual.contains(fileText)) throw new RuntimeException("text from file" + i + " not found in actual-results.txt")
 }
